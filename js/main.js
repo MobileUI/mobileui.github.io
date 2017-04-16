1614937436345d4615c514eb61c9b9f23a4725d9
Fabio <fabio.rogerio.sj@gmail.com>
2017-04-16 00:54:29 -0300

    init

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
   $('textarea.bind-code').each(function(i, block) {
     var mode = $(block).attr('mode')
+    var absolute = $(block).attr('header-absolute')
     if(!mode) {
       mode = 'text/html'
     }
-    CodeMirror.fromTextArea(block, {
+    var editor = CodeMirror.fromTextArea(block, {
       lineNumbers: false,
       mode: mode,
       readOnly: true
     });
+    var code = $(block).val()
+    if(absolute) {
+      code = code.replace('"header', '"header header-absolute')
+    }
+    $(block).after('<div class="result">'+code+'<div class="cls"></div></div>')
   });
 
   var $document = $(document);
